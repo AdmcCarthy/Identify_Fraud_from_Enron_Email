73cd577b263855cd47997bc61e1d0cd1b327171d
Adam James McCarthy <a.mccarthy.uk@gmail.com>
2017-08-01 09:52:26 +0200

    style: Added doc string to functions

--- final_project/learnEnron/feature_engineering.py
+++ final_project/learnEnron/feature_engineering.py
@@ -12,12 +12,29 @@ import numpy as np
 
 def email_ratios(datadict):
     """
+    Add new features into the data
+    dictionary based on email data.
+
+    Create a ratio based on the number
+    of emails sent to a person and the
+    number of emails sent to this person
+    from a person of interst (POI).
+
+    Create a ratio based on the number of
+    emails from a person and the number of
+    emails this person to sent to a POI.
 
     Parameters
     ----------
-
+    datadict = dictionary
+        A dictionary storing all of the dataset.
+        Each key relates to a person while the value
+        is a dictionary containing all of the variables.
     Returns
     -------
+    data_dict = dictionary
+        Almost identical to input but with two new variables
+        added.
     """
 
     # Convert data dictionary, tranpose
@@ -27,8 +44,14 @@ def email_ratios(datadict):
 
     df = df.transpose()
 
-    df["ratio_to_poi"] = df["from_this_person_to_poi"].dropna()/df["from_messages"].dropna()
-    df["ratio_from_poi"] = df["from_poi_to_this_person"].dropna()/df["to_messages"].dropna()
+    df["ratio_to_poi"] = (
+                          df["from_this_person_to_poi"].dropna()
+                          /df["from_messages"].dropna()
+                          )
+    df["ratio_from_poi"] = (
+                            df["from_poi_to_this_person"].dropna()
+                            /df["to_messages"].dropna()
+                            )
 
     df = df.replace(np.NaN, 0)
 
