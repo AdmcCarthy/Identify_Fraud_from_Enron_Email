1b483291a5245220b06a6cfc4613bc30950e319f
Adam James McCarthy <a.mccarthy.uk@gmail.com>
2017-08-02 09:44:49 +0200

    feat: Added logistic regression to module

--- final_project/learnEnron/tune.py
+++ final_project/learnEnron/tune.py
@@ -13,13 +13,16 @@
 """
 
 from sklearn.model_selection import GridSearchCV, StratifiedKFold
+from sklearn import ensemble, linear_model
 
 
-def param_optimize(features, labels, clf, grid_search=True):
+def param_optimize_gb(features, labels, grid_search=True):
     """
     Hyper parameter optimization
     through parameter grid search
     during cross validation.
+
+    Tailored for a GradientBoosting classifer.
 
     Parameters
     ----------
@@ -27,12 +30,6 @@ def param_optimize(features, labels, clf, grid_search=True):
         Containing the data within a dictionary
     feature_list = list
         Contains all varibles to be scaled.
-    clf = sklearn clf object
-        clf type to be applied during grid search
-    params = dict
-        Parameters to be used in GridSearchCv,
-        must be suitable for the choosen
-        classifier.
 
     Returns
     -------
@@ -48,6 +45,8 @@ def param_optimize(features, labels, clf, grid_search=True):
     # Which metric should be used to optimize the
     # cross validation
     score = "f1_weighted"
+
+    clf = ensemble.GradientBoostingClassifier()
 
     parameters = [{
                    "loss": ["deviance", "exponential"],
@@ -83,5 +82,79 @@ def param_optimize(features, labels, clf, grid_search=True):
     clf.fit(features, labels)
 
     print("Best classifier score:", clf.best_score_, ":", clf.best_params_)
+
+    clf = clf.best_estimator_
+
+    return clf
+
+
+def param_optimize_gb(features, labels, grid_search=True):
+    """
+    Hyper parameter optimization
+    through parameter grid search
+    during cross validation.
+
+    Tailored for a Logistic Regression classifer.
+
+    Parameters
+    ----------
+    datadict = dict
+        Containing the data within a dictionary
+    feature_list = list
+        Contains all varibles to be scaled.
+
+    Returns
+    -------
+    clf_f = sklearn clf object
+        Fitted classifier including the hyper
+        parameter optimization.
+    """
+
+        # How many splits
+    n = 2
+    cv = StratifiedKFold(n_splits=n, shuffle=True)
+
+    # Which metric should be used to optimize the
+    # cross validation
+    score = "f1_weighted"
+
+    clf = ensemble.GradientBoostingClassifier()
+
+    parameters = [{
+                   "loss": ["deviance", "exponential"],
+                   "n_estimators": [120, 300, 500, 800, 1200],
+                   "max_depth": [3, 5, 7, 9, 12, 15, 17, 25],
+                   "min_samples_split": [2, 5, 10, 15, 100],
+                   "min_samples_leaf": [2, 5, 10],
+                   "subsample": [0.6, 0.7, 0.8, 0.9, 1],
+                   "max_features": ["sqrt", "log2", None]
+                   }]
+
+    # Parameters based on best results from exhaustive grid
+    # search of all parameters.
+    if grid_search is not True:
+        parameters = [{
+                       'subsample': [0.8],
+                       'n_estimators': [120],
+                       'max_depth': [25],
+                       'loss': ['deviance'],
+                       'min_samples_split': [2],
+                       'min_samples_leaf': [2],
+                       'max_features': ['sqrt']
+                       }]
+
+    clf = GridSearchCV(
+                       estimator=clf,
+                       param_grid=parameters,
+                       cv=cv,
+                       scoring=score
+                       )
+
+    # Will take time...
+    clf.fit(features, labels)
+
+    print("Best classifier score:", clf.best_score_, ":", clf.best_params_)
+
+    clf = clf.best_estimator_
 
     return clf
