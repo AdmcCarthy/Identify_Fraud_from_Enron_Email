687eb9fc3527eac2bb48023be794f753de5df9b3
Adam James McCarthy <a.mccarthy.uk@gmail.com>
2017-08-02 14:35:40 +0200

    feat: Completed logistic regression

--- final_project/learnEnron/tune.py
+++ final_project/learnEnron/tune.py
@@ -88,7 +88,7 @@ def param_optimize_gb(features, labels, grid_search=True):
     return clf
 
 
-def param_optimize_gb(features, labels, grid_search=True):
+def param_optimize_lr(features, labels, grid_search=True, folds=2):
     """
     Hyper parameter optimization
     through parameter grid search
@@ -110,37 +110,25 @@ def param_optimize_gb(features, labels, grid_search=True):
         parameter optimization.
     """
 
-        # How many splits
-    n = 2
+    # How many splits
+    n = folds
     cv = StratifiedKFold(n_splits=n, shuffle=True)
 
     # Which metric should be used to optimize the
     # cross validation
     score = "f1_weighted"
 
-    clf = ensemble.GradientBoostingClassifier()
+    clf = linear_model.LogisticRegression()
 
     parameters = [{
-                   "loss": ["deviance", "exponential"],
-                   "n_estimators": [120, 300, 500, 800, 1200],
-                   "max_depth": [3, 5, 7, 9, 12, 15, 17, 25],
-                   "min_samples_split": [2, 5, 10, 15, 100],
-                   "min_samples_leaf": [2, 5, 10],
-                   "subsample": [0.6, 0.7, 0.8, 0.9, 1],
-                   "max_features": ["sqrt", "log2", None]
+                   "C": [0.001, 0.01, 0.1, 1, 10, 100, 1000],
                    }]
 
     # Parameters based on best results from exhaustive grid
     # search of all parameters.
     if grid_search is not True:
         parameters = [{
-                       'subsample': [0.8],
-                       'n_estimators': [120],
-                       'max_depth': [25],
-                       'loss': ['deviance'],
-                       'min_samples_split': [2],
-                       'min_samples_leaf': [2],
-                       'max_features': ['sqrt']
+                       "C": [10]
                        }]
 
     clf = GridSearchCV(
